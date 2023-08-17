5d0e74f0077f4f192381415c8069ce4c7caa5dab
Scott Randolph <111802200+FieldBoss@users.noreply.github.com>
2023-08-17 08:17:41 -0600

    Add files via upload

---
+++ src/components/point_load_child.js
@@ -0,0 +1,126 @@
+
+const pointLoad = {
+  "loader": [
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Degraded",
+            "survival": "Medium",
+            "richness": "High",
+            "production": "N/A",
+            "diversity": "High", 
+            "inspector": "Jimmy"
+          },
+
+          "geometry": [
+            {"lat":-3.141073,"lng":-58.982892}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Healthy",
+            "survival": "Medium",
+            "richness": "High",
+            "production": "Low",
+            "diversity": "High", 
+            "inspector": "Woody"
+          },
+
+          "geometry": [
+            {"lat":-3.142059,"lng":-58.981991}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Threatened",
+            "survival": "Medium",
+            "richness": "Low",
+            "production": "Medium",
+            "diversity": "Low", 
+            "inspector": "Batiste"
+          },
+
+          "geometry": [
+            {"lat":-3.143901,"lng":-58.983364}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Degraded",
+            "survival": "Medium",
+            "richness": "High",
+            "production": "N/A",
+            "diversity": "High", 
+            "inspector": "Jimmy"
+          },
+
+          "geometry": [
+            {"lat":-3.144501,"lng":-58.985124}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Healthy",
+            "survival": "Medium",
+            "richness": "High",
+            "production": "Low",
+            "diversity": "High", 
+            "inspector": "Woody"
+          },
+
+          "geometry": [
+            {"lat":-3.146044,"lng":-58.986111}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Threatened",
+            "survival": "Medium",
+            "richness": "Low",
+            "production": "Medium",
+            "diversity": "Low", 
+            "inspector": "Batiste"
+          },
+
+          "geometry": [
+            {"lat":-3.146258,"lng":-58.983965}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Healthy",
+            "survival": "Medium",
+            "richness": "High",
+            "production": "Low",
+            "diversity": "High", 
+            "inspector": "Woody"
+          },
+
+          "geometry": [
+            {"lat":-3.147244,"lng":-58.982677}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Threatened",
+            "survival": "Medium",
+            "richness": "Low",
+            "production": "Medium",
+            "diversity": "Low", 
+            "inspector": "Batiste"
+          },
+
+          "geometry": [
+            {"lat":-3.148915,"lng":-58.984137}
+          ]
+        }
+    ]
+}
+export default pointLoad;
---
+++ src/components/poly_load_child.js
@@ -0,0 +1,78 @@
+
+const polyLoad = {
+  "loader": [
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Healthy",
+            "survival": "Medium",
+            "richness": "High",
+            "production": "N/A",
+            "diversity": "High", 
+            "inspector": "Joey"
+          },
+
+          "geometry": [
+	          {"lat":-12.974859227,"lng":-71.48257662099999},     
+	          {"lat":-12.974859227,"lng":-71.47957662099999},   
+	          {"lat":-12.971859227,"lng":-71.47957662099999},   
+	          {"lat":-12.971859227,"lng":-71.48257662099999}    
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Degraded",
+            "survival": "High",
+            "richness": "High",
+            "production": "N/A",
+            "diversity": "Low", 
+            "inspector": "Jimmy"
+          },
+
+          "geometry": [
+            {"lat":-12.971859227,"lng":-71.48257662099999},
+            {"lat":-12.971859227,"lng":-71.47957662099999},
+            {"lat":-12.968859227,"lng":-71.47957662099999},
+            {"lat":-12.968859227,"lng":-71.48257662099999}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Threatened",
+            "survival": "Medium",
+            "richness": "N/A",
+            "production": "N/A",
+            "diversity": "Medium", 
+            "inspector": "Johnny"
+          },
+
+          "geometry": [
+            {"lat":-12.971859227,"lng":-71.48557662099999},
+            {"lat":-12.971859227,"lng":-71.48257662099999},
+            {"lat":-12.968859227,"lng":-71.48257662099999},
+            {"lat":-12.968859227,"lng":-71.48557662099999}
+          ]
+        },
+        {
+          "properties": {
+            "masterid": "",
+            "condition": "Degraded",
+            "survival": "Low",
+            "richness": "Low",
+            "production": "N/A",
+            "diversity": "N/Ah", 
+            "inspector": "Jimmy"
+          },
+
+          "geometry": [
+	          {"lat":-12.968859227,"lng":-71.48557662099999},
+	          {"lat":-12.968859227,"lng":-71.48257662099999},
+	          {"lat":-12.965859227,"lng":-71.48257662099999},
+	          {"lat":-12.965859227,"lng":-71.48557662099999}
+          ]
+        }
+    ]
+}
+export default polyLoad;
